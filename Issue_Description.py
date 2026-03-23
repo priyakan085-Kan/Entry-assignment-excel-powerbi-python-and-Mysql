2fe0fa380168f1b91206c563cddde7e690859f81
priyakan085-Kan <priyakan085@gmail.com>
2026-03-23 17:56:21 +0530

    Entry  Assignments -Excel-PowerBI and Mysql
    Entry  Assignments-Excel-PowerBI and Mysql

---
+++ Issue_Description.py
@@ -0,0 +1,7 @@
+ 'Issue_Description';
+  [
+        ' Internet not working!!! ', 'slow response, very poor service ',
+        'GREAT support! issue resolved.',' okay... need help ', 'not BAD but slow', 'Excellent guidance, Very Helpful!', 'good support and good behaviour!', 'Poor handling of technical issue', 'Satisfied. Could be better.',  'Good service... quick response.'
+  ],  
+  'Priority': ['High', 'Low', 'High', 'Medium', 'Low', 'High', 'Medium', 'High', 'Low', 'Medium']}
+print(ticket_data)
